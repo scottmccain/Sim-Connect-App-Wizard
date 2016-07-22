ec38a1684a019d6fbc2374ffc9e6f506eaf2200b
Scott McCain <scott.mccain@gmail.com>
2016-07-21 19:31:49 -0700

    Final Version

--- SimConnect Application Wizard/Templates/1033/Application.cpp
+++ SimConnect Application Wizard/Templates/1033/Application.cpp
@@ -4,3 +4,25 @@
 [!endif]
 
 #include "[!output APPLICATION_HEADER]"
+
+
+BEGIN_DISPATCH_MAP([!output SAFE_APPCLASS_NAME])
+END_DISPATCH_MAP
+
+[!output SAFE_APPCLASS_NAME]::[!output SAFE_APPCLASS_NAME](HINSTANCE hInstance, LPCSTR szAppName) : CSimConnectApp(hInstance, szAppName)
+{
+}
+
+[!output SAFE_APPCLASS_NAME]::~[!output SAFE_APPCLASS_NAME]()
+{
+
+}
+
+HRESULT [!output SAFE_APPCLASS_NAME]::OnConnect()
+{
+	HRESULT hr = S_OK;
+
+	// TODO: Add Initialization code here
+
+	return hr;
+}
--- SimConnect Application Wizard/Templates/1033/Application.h
+++ SimConnect Application Wizard/Templates/1033/Application.h
@@ -1,2 +1,17 @@
 // TODO: add application stuff
 // [!output SAFE_APPCLASS_NAME]
+
+#include "SimConnectApp.h"
+
+class [!output SAFE_APPCLASS_NAME] : public CSimConnectApp
+{
+
+protected:
+	virtual HRESULT OnConnect();
+
+	DECLARE_DISPATCH_MAP
+
+public:
+	[!output SAFE_APPCLASS_NAME](HINSTANCE, LPCSTR);
+	virtual ~[!output SAFE_APPCLASS_NAME](void);
+};
